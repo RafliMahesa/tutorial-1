b4ed2bc181c4e5937e8cc34c413a53a6adc99ce1
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-02-05 22:17:21 +0700

    add edit interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -5,5 +5,7 @@ import java.util.List;
 
 public interface ProductService {
     public Product create(Product product);
+    public Product search(int id);
+    public Product edit(Product product);
     public List<Product> findAll();
 }
